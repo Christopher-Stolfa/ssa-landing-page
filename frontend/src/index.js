192c48a7e5ab3ed3a6284208528468132cccae10
Christopher Stolfa <xcstolfax@gmail.com>
2022-07-08 13:54:12 -0700

    refactor: Make id root

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 
-const root = ReactDOM.createRoot(document.getElementById('ssa_landing_page'));
-
-const target = document.getElementById('ssa_landing_page');
-target &&
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
