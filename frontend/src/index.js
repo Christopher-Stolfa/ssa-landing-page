bab5029f8b86719baebe883dc54913163a91aa44
Christopher Stolfa <xcstolfax@gmail.com>
2022-07-08 10:49:47 -0700

    chore: Configure as plugin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('ssa_landing_page'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+
+const target = document.getElementById('ssa_landing_page');
+target &&
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
