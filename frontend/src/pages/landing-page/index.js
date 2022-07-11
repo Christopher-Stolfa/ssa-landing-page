35d2dfdc59aed829fd0255caefc0f65b66aa7e14
Christopher Stolfa <xcstolfax@gmail.com>
2022-07-11 15:29:30 -0700

    fix: Use pad if device screen is smaller than 1024px

--- frontend/src/pages/landing-page/index.js
+++ frontend/src/pages/landing-page/index.js
@@ -2,16 +2,23 @@ import styles from './landing-page.module.css';
 import Header from '../../components/headers/header';
 import Hero from '../../components/hero/hero';
 import Offers from './offers';
+import useTheme from '../../hooks/useTheme';
 
-const LandingPage = () => (
-  <>
-    <Header />
-    <main id="main" className={styles.container}>
-      <div className={styles.pad} />
-      <Hero />
-      <Offers />
-    </main>
-  </>
-);
+const LandingPage = () => {
+  const {
+    device,
+    deviceTypes: { desktop },
+  } = useTheme();
+  return (
+    <>
+      <Header />
+      <main id="main" className={styles.container}>
+        {device !== desktop && <div className={styles.pad} />}
+        <Hero />
+        <Offers />
+      </main>
+    </>
+  );
+};
 
 export default LandingPage;
