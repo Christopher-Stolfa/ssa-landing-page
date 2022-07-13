85a912ff0cf73acb04110a5ee5a6b5a3a26dd7f4
Christopher Stolfa <xcstolfax@gmail.com>
2022-07-12 20:43:08 -0700

    feat: get page assets from different paths based on env

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import App from './app';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Helmet } from 'react-helmet';
 
+const assetsPath = '/wp-content/themes/ssa/landing-page-assets/';
+const isProduction = process.env.NODE_ENV === 'production';
+
 const client = new ApolloClient({
   uri: 'https://dev-spitzer-arch.pantheonsite.io/graphql',
   cache: new InMemoryCache(),
@@ -19,15 +22,21 @@ root.render(
   <>
     <Helmet>
       <meta charSet="utf-8" />
-      <link rel="icon" href={`${process.env.PUBLIC_URL}/`} />
+      <link rel="icon" href={isProduction ? assetsPath + 'favicon.ico' : process.env.PUBLIC_URL + '/favicon.ico'} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       <meta
         name="description"
         content="The Bernard and Anne Spitzer School of Architecture, New York City's flagship public school for architecture."
       />
-      <link rel="apple-touch-icon" href={`${process.env.PUBLIC_URL}/apple-touch-icon.png`} />
-      <link rel="manifest" href={`${process.env.PUBLIC_URL}/manifest.json`} />
+      <link
+        rel="apple-touch-icon"
+        href={isProduction ? assetsPath + 'apple-touch-icon.png' : process.env.PUBLIC_URL + '/apple-touch-icon.png'}
+      />
+      <link
+        rel="manifest"
+        href={isProduction ? assetsPath + 'manifest.json' : process.env.PUBLIC_URL + '/manifest.json'}
+      />
       <title>The Bernard and Anne Spitzer School of Architecture landing page</title>
       <link
         href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Roboto+Condensed:400,700"
