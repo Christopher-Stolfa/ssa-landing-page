fdffe7ad407912a65d1791b06f28d2b47739c581
Christopher Stolfa <xcstolfax@gmail.com>
2022-07-12 17:17:51 -0700

    refactor

--- frontend/src/components/headers/header.js
+++ frontend/src/components/headers/header.js
@@ -17,6 +17,8 @@ const Header = () => {
 
   // If app is loading or there graphql error use static menu options as a fallback.
   if (loading || error) {
+    error && console.log('There has been a graphQL error with the menu.');
+
     return (
       <header id="header">
         {device === deviceTypes.desktop ? (
@@ -26,9 +28,6 @@ const Header = () => {
         )}
       </header>
     );
-  }
-  if (error) {
-    console.log('There has been a graphQL error with the menu.');
   }
   // If the data has been received
   if (data) {
