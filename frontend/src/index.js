8c507d3e3f0724ad4c877ec846bec6434ab92ef0
Christopher Stolfa <xcstolfax@gmail.com>
2022-07-12 20:18:43 -0700

    fix: Rely on react-helmet to set document head instead of index.html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { Helmet } from 'react-helmet';
 
 const client = new ApolloClient({
   uri: 'https://dev-spitzer-arch.pantheonsite.io/graphql',
@@ -13,10 +14,31 @@ const client = new ApolloClient({
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>,
+  <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <link rel="icon" href={`${process.env.PUBLIC_URL}/`} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="The Bernard and Anne Spitzer School of Architecture, New York City's flagship public school for architecture."
+      />
+      <link rel="apple-touch-icon" href={`${process.env.PUBLIC_URL}/apple-touch-icon.png`} />
+      <link rel="manifest" href={`${process.env.PUBLIC_URL}/manifest.json`} />
+      <title>The Bernard and Anne Spitzer School of Architecture landing page</title>
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Roboto+Condensed:400,700"
+        rel="stylesheet"
+        type="text/css"
+      />
+    </Helmet>
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </React.StrictMode>
+  </>,
 );
