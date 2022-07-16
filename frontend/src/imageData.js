a48ee65e90c4dabc5962001c1a5da534ea6084b4
Christopher Stolfa <xcstolfax@gmail.com>
2022-07-16 16:04:14 -0700

    fix: Use build folder assets instead of landing-page-assets

--- frontend/src/imageData.js
+++ frontend/src/imageData.js
@@ -1,63 +1,63 @@
 /**
  * This path only uses wordpress path to image assets in production build.
  */
-const path = process.env.NODE_ENV !== 'production' ? '' : '/wp-content/themes/ssa/landing-page-assets/';
+// const path = process.env.NODE_ENV !== 'production' ? '' : '/wp-content/themes/ssa/landing-page-assets/';
 
 /**
  * ssa logo
  */
-const ssaLogoSrc = path + 'images/spitzer_logo.svg';
+const ssaLogoSrc = 'images/spitzer_logo.svg';
 
 /**
  * ccny logo
  */
-const ccnyLogoSrc = path + 'images/logo-ccny-horz-w.svg';
+const ccnyLogoSrc = 'images/logo-ccny-horz-w.svg';
 /**
  * ccny logo 2
  */
-const ccnyLogo2Src = path + 'images/ccny.svg';
+const ccnyLogo2Src = 'images/ccny.svg';
 /**
  * cuny logo
  */
-const cunyLogoSrc = path + 'images/cuny.svg';
+const cunyLogoSrc = 'images/cuny.svg';
 /**
  * Digital tour thumbnail
  */
-const digitourSrc = path + 'images/digitour-thumbnail.jpg';
+const digitourSrc = 'images/digitour-thumbnail.jpg';
 /**
  * ssa small logl
  */
-const ssaSmallLogoSrc = path + 'images/ssa_small.svg';
+const ssaSmallLogoSrc = 'images/ssa_small.svg';
 /**
  * Image data used header backdrop
  */
 const backgroundImages = [
-  { src: path + 'background-images/image-1.jpg' },
-  { src: path + 'background-images/image-2.jpg' },
-  { src: path + 'background-images/image-3.jpg' },
-  { src: path + 'background-images/image-4.jpg' },
-  { src: path + 'background-images/image-5.jpg' },
-  { src: path + 'background-images/image-6.jpg' },
+  { src: 'background-images/image-1.jpg' },
+  { src: 'background-images/image-2.jpg' },
+  { src: 'background-images/image-3.jpg' },
+  { src: 'background-images/image-4.jpg' },
+  { src: 'background-images/image-5.jpg' },
+  { src: 'background-images/image-6.jpg' },
 ];
 /**
  * Offers page background image
  */
-const offersBackgroundSrc = path + 'images/fav-bySirinSamman-1448-X3.jpg';
+const offersBackgroundSrc = 'images/fav-bySirinSamman-1448-X3.jpg';
 /**
  * Image data used for slide show
  */
 const slideImages = [
   {
-    src: path + 'slideshow-images/fav-bySirinSamman-0256.jpg',
+    src: 'slideshow-images/fav-bySirinSamman-0256.jpg',
     alt: 'SSA students working on projects. Photo taken by Sirin Samman.',
   },
-  { src: path + 'slideshow-images/image_19.jpg', alt: 'ssa campus image' },
+  { src: 'slideshow-images/image_19.jpg', alt: 'ssa campus image' },
   {
-    src: path + 'slideshow-images/fav-bySirinSamman-0074.jpg',
+    src: 'slideshow-images/fav-bySirinSamman-0074.jpg',
     alt: 'SSA student presentations. Photo taken by Sirin Samman.',
   },
   {
-    src: path + 'slideshow-images/fav-bySirinSamman-0230.jpg',
+    src: 'slideshow-images/fav-bySirinSamman-0230.jpg',
     alt: 'SSA students having a discussion with a professor. Photo taken by Sirin Samman.',
   },
 ];
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Helmet } from 'react-helmet';
 import { ThemeProvider } from './context/ThemeContext';
 
-const assetsPath = '/wp-content/themes/ssa/landing-page-assets/';
-const isProduction = process.env.NODE_ENV === 'production';
+// const assetsPath = '/wp-content/themes/ssa/landing-page-assets/';
+// const isProduction = process.env.NODE_ENV === 'production';
 
 const client = new ApolloClient({
   uri: 'https://dev-spitzer-arch.pantheonsite.io/graphql',
@@ -25,22 +25,22 @@ root.render(
   <>
     <Helmet>
       <meta charSet="utf-8" />
-      <link rel="icon" href={isProduction ? assetsPath + 'favicon.ico' : process.env.PUBLIC_URL + '/favicon.ico'} />
+      <link rel="icon" href={process.env.PUBLIC_URL + '/favicon.ico'} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       <meta
         name="description"
         content="The Bernard and Anne Spitzer School of Architecture, New York City's flagship public school for architecture."
       />
-      <link
-        rel="apple-touch-icon"
-        href={isProduction ? assetsPath + 'apple-touch-icon.png' : process.env.PUBLIC_URL + '/apple-touch-icon.png'}
+      <link rel="apple-touch-icon" href={process.env.PUBLIC_URL + '/apple-touch-icon.png'} />
+      <link rel="manifest" href={process.env.PUBLIC_URL + '/manifest.json'} />
+      <title>The Bernard and Anne Spitzer School of Architecture</title>
+      <meta property="og:title" content="The Bernard and Anne Spitzer School of Architecture" />
+      <meta
+        property="og:description"
+        content="The Bernard and Anne Spitzer School of Architecture, New York City's flagship public school for architecture."
       />
-      <link
-        rel="manifest"
-        href={isProduction ? assetsPath + 'manifest.json' : process.env.PUBLIC_URL + '/manifest.json'}
-      />
-      <title>The Bernard and Anne Spitzer School of Architecture landing page</title>
+      <meta name="author" content="Christopher Stolfa" />
       <link
         href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Roboto+Condensed:400,700"
         rel="stylesheet"
