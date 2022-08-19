38e4c6aff65597e2b1eb39294b719cbd0c294866
Christopher Stolfa <xcstolfax@gmail.com>
2022-08-19 13:36:33 -0700

    feat: Create index file for exporting landing-page. Add landing-page component to its own file

---
+++ src/pages/landing-page/landing-page.js
@@ -0,0 +1,41 @@
+import styles from './landing-page.module.css';
+import Header from '../../components/headers/header';
+import Hero from '../../components/hero/hero';
+import Offers from './offers';
+import useTheme from '../../hooks/useTheme';
+import VideoContainer from '../../components/video-container/video-container';
+import { digitalTourVideo } from '../../data';
+import Footer from '../../components/footers/footer';
+import SkipLinks from '../../components/skip-links/skip-links';
+import News from './news';
+import Calendar from './calendar';
+
+const LandingPage = () => {
+  const {
+    device,
+    deviceTypes: { desktop },
+  } = useTheme();
+
+  return (
+    <>
+      <SkipLinks />
+      <Header />
+      <main id="main-content" className={styles.container}>
+        {device !== desktop && <div style={{ height: '3.5rem' }} />}
+        <Hero />
+        <Offers />
+        <div className={styles.flexBackground}>
+          <div className={styles.flex}>
+            <News />
+            <Calendar />
+          </div>
+        </div>
+
+        <VideoContainer url={digitalTourVideo} />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default LandingPage;
